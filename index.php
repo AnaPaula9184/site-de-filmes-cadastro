5f280ccf28aa48144c4bc609a071cc3db6744d96
Ana Soares <pauladeveloper8@gmail.com>
2022-09-13 16:38:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>CLARACINE</title>
+</head>
+
+<?php include "cabecalho.php" ?>
+
+<body>
+
+
+<nav class="nav-extended pink lighten-3">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+     </ul>
+    </div>
+    <div class="nav-header center">
+       <h1>CLARACINE</h1>
+      </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent pink">
+        <li class="tab"><a href="active">Todos</a></li>
+        <li class="tab"><a class="#test2" href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
+     
+  <div class="row">
+      <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/original/rpK3dukkvCa3E7iOPphsBGTO2dS.jpg">
+          <span class="card-title"></span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+             <p class="valign-wrapper">
+              <i class="material-icons amber-text">star</i>9.0</p>
+          <p>Um cão de caça sem sorte se encontra em uma cidade cheia de gatos que precisam de um herói para defendê-los do plano maligno de um vilão implacável para varrer sua vila do mapa. Com a ajuda de um treinador relutante, o azarão deve assumir o papel de temível samurai e se juntar aos felinos para salvar o dia.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/original/FiqBRypTDyHzqcxdbpPb3ddFep.jpg">
+          <span class="card-title"></span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+             <p class="valign-wrapper">
+              <i class="material-icons amber-text">star</i>9.7</p>
+          <p>Com suas capas balançando, Gamma 1 e Gamma 2 podem ser vistos fazendo algumas poses incríveis! Além disso, há alguns novos personagens originais que teremos que esperar para descobrir mais, não importa o quão difícil possa ser! Vestindo seu uniforme e tomando o centro do palco está Gohan, que parece estar se preparando para a luta.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/original/aLeWIMkBVGRqYVWtP1aKcWfYNN2.jpg">
+          <span class="card-title"></span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+             <p class="valign-wrapper">
+              <i class="material-icons amber-text">star</i>7.1</p>
+          <p>Depois de se estabelecer em Green Hills, Sonic está pronto para mais liberdade e deixar sua marca como um herói, e Tom e Maddie concordam em deixá-lo em casa enquanto vão de férias. Mas, assim que eles se foram, Dr. Robotnik volta, desta vez com um novo parceiro, Knuckles, em busca de uma esmeralda que tem o poder.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/original/bapLpnoV3PUbgcZNDwg6Hxe75HI.jpg">
+          <span class="card-title"></span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+             <p class="valign-wrapper">
+              <i class="material-icons amber-text">star</i>9.5</p>
+          <p>Um Minion, vendo muitos donos passearem com seus cachorros, quer um filhote também. Ele tenta arrumar uma joaninha, mas não dá certo. Felizmente o UFO que expulsa a joaninha, de alguma forma, concorda em se tornar um Bichinho de Estimação.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  
+
+
+    
+</body>
+</html>
